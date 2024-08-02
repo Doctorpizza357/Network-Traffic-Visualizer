2d9efd25ef4c134a6c66a78182fdda061180681d
Tomas Bentolila <tomasbentolila@gmail.com>
2024-08-02 14:11:51 -0400

    initial commit

--- Visualizer.py
+++ Visualizer.py
@@ -3,10 +3,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from collections import Counter
 
-# Initialize a list to store captured packet data
 data = []
 
-# Callback function to process captured packets
 def packet_callback(packet):
     if IP in packet:
         ip_src = packet[IP].src
@@ -20,20 +18,15 @@ def packet_callback(packet):
             "Size": packet_size
         })
 
-# Start packet capture
 print("Starting packet capture...")
 sniff(prn=packet_callback, count=100)
 
-# Convert captured data to a Pandas DataFrame
 df = pd.DataFrame(data)
 
-# Count the number of packets by source IP
 src_counts = Counter(df['Source'])
 
-# Convert Counter to Pandas Series for easy plotting
 src_counts_series = pd.Series(src_counts)
 
-# Plot the number of packets by source IP
 plt.figure(figsize=(10, 5))
 src_counts_series.plot(kind='bar')
 plt.title('Number of Packets by Source IP')
